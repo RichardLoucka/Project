569f08cdc7e035ae489cd26355dd60cd6ec49245
Richard Loučka <richardbloucka@gmail.com>
2025-02-18 18:14:41 +0100

    Pridani File do tabulky

---
+++ TaskManager/Migrations/20250218162511_new.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class @new : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "File",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "File",
+                table: "Tasks");
+        }
+    }
+}
