43f4d0fdefab9adab2eba43a8f35e9de07c58710
Richard Loučka <richardbloucka@gmail.com>
2025-02-18 16:03:52 +0100

    Přidání třídy Task.

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Task
+    {
+        [Key]
+        private int id{get;set;}
+        public string Title{get;set;}
+        public string Description{get;set;}
+        public int Priority{get;set;}
+    }
+}
