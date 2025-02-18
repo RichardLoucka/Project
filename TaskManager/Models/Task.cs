6831d32d41d7e03d18c74f5ac3b2b83217efb584
Richard Loučka <richardbloucka@gmail.com>
2025-02-18 16:50:34 +0100

    Úprava třídy Task(setter, primary), vypis obsahu tabulky do view

--- TaskManager/Models/Task.cs
+++ TaskManager/Models/Task.cs
@@ -4,8 +4,13 @@ namespace TaskManager.Models
 {
     public class Task
     {
-        [Key]
-        private int id{get;set;}
+        [Key] private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
         public string Title{get;set;}
         public string Description{get;set;}
         public int Priority{get;set;}
